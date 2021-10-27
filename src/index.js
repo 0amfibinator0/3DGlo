0ddc48540c74a9e77a6f880fd2ee0cd2ff422eb3
0amfibinator0 <00amfibiantor00@mail.ru>
2021-10-27 21:02:25 +0300

    задание 20 урока сделано

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import timer from './modules/timer';
+import menu from './modules/menu';
+import modal from './modules/modal';
 
 setInterval(timer, 1000, '30 october 2021');
 timer('30 october 2021');
+menu();
+modal();
