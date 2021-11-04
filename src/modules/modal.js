027209e42006505e1935687c5fb611aeccc336af
0amfibinator0 <00amfibiantor00@mail.ru>
2021-11-04 22:19:29 +0300

    задание 25 урока сделано

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,31 +1,45 @@
+import {animate} from './helpers';
+
 const modal = () => {
     const modal = document.querySelector('.popup');
     const buttons = document.querySelectorAll('.popup-btn');
     const popup = modal.querySelector('.popup-content');
-    
-    popup.style.opacity = '0';
 
     window.addEventListener("resize", modalFunc);
     
     function modalFunc () {
         if (document.documentElement.clientWidth >= 769) {
-        buttons.forEach(btn => {
-            btn.addEventListener('click', () => {
-                modal.style.display = 'block';
-                setTimeout(() => {
-                    popup.style.transition = '0.5s';
-                    popup.style.opacity = '1';
-                }, 200);
+            buttons.forEach(btn => {
+                btn.addEventListener('click', () => {
+                    animate({
+                        duration: 1000,
+                        timing(timeFraction) {
+                            return timeFraction;
+                        },
+                        draw(progress) {
+                            modal.style.display = 'block';
+                            popup.style.transition = '0.2s';
+                            popup.style.opacity = progress;
+                        }
+                    });
+                });
             });
-        });
         
         modal.addEventListener('click', (e) => {
             if (!e.target.closest('.popup-content') || e.target.classList.contains('popup-close')) {
-                popup.style.transition = '1s';
-                popup.style.opacity = '0';
-                setTimeout(() => {
-                    modal.style.display = 'none';
-                }, 500);
+                animate({
+                    duration: 1000,
+                    timing(timeFraction) {
+                        return timeFraction;
+                    },
+                    draw(progress) {
+                        popup.style.transition = '0.2s';
+                        popup.style.opacity = progress;
+                        setTimeout(() => {
+                            modal.style.display = 'none';
+                        }, 500);
+                    }
+                });
             }
         });
         } else {
