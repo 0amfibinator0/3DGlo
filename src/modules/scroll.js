b8c3c550958c38ba84d7e6e2e4f06326136bc5eb
0amfibinator0 <00amfibiantor00@mail.ru>
2022-02-04 21:58:29 +0300

    теперь точно

--- src/modules/scroll.js
+++ src/modules/scroll.js
@@ -1,5 +1,18 @@
 const headerScroll = () => {
     const btn = document.getElementById('scroll-btn');
+    const link = document.querySelectorAll('#modal-link');
+
+    link.forEach((el) => {
+        el.addEventListener('click', function (e) {
+            e.preventDefault();
+            
+            const blockID = el.getAttribute('href');
+            
+            document.querySelector(blockID).scrollIntoView({
+                behavior: 'smooth',
+            });
+        });
+    });
 
     btn.addEventListener('click', function (e) {
         e.preventDefault();
